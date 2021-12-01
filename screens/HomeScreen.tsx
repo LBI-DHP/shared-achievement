d54f298f42924b2eedaeca889c1bf33b13fc231c
Florian Katsch <florian.katsch@dhp.lbg.ac.at>
2021-12-01 12:17:38 +0100

    persistent user id, again

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -6,16 +6,26 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 
 import { style } from "../constants/Styles";
+import dataManager from "../components/DataManager"
 
-import { useEffect, useState } from 'react'; 
+import { useEffect, useState } from 'react';
 
 import configJSON from "../config.json";
 
 export default function HomeScreen() {
   const [username, setName] = React.useState(""); // TODO replace with active username
-  const [userid, setUserID] = React.useState("123456789");
   const [userteam, setUserTeam] = React.useState("");
   const [teamSteps, setTeamSteps] = React.useState("");
+
+  const [userid, setUserID] = React.useState(null);
+  dataManager.getUserId().then(
+      (id) => {
+        setUserID(id)
+        console.log("UserId is: " + id)
+      })
+    .catch(
+      (e) => console.log("error")
+  )
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);

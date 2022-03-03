9503e6ef5d2028e2e12f276a1693271aa3452040
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2022-03-03 17:25:01 +0100

    read db configutation from .env

--- backend/webapp/app.py
+++ backend/webapp/app.py
@@ -1,13 +1,25 @@
 import os
-
+from environs import Env
 from flask import Flask
 from flask_peewee.db import Database
+from config import Configuration
 # from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
+conf = Configuration()
+
+env = Env()
+env.read_env()
+
+conf.DATABASE['name'] = env('DB_NAME')
+conf.DATABASE['user'] = env('DB_USER')
+conf.DATABASE['password'] = env('DB_PASS')
+
+
 # app.config.from_object('settings')
-app.config.from_object('config.Configuration')
+# app.config.from_object('config.Configuration')
+app.config.from_object(conf)
 
 db = Database(app)
 
--- backend/webapp/config.py
+++ backend/webapp/config.py
@@ -1,3 +1,4 @@
+
 class Configuration(object):
     DATABASE = {
         'name': 'shared_achievement_db',
@@ -6,5 +7,6 @@ class Configuration(object):
 	    'password':'lbi',
         'engine': 'peewee.MySQLDatabase',
     }
+
     DEBUG = True
     SECRET_KEY = 'kjkWLIO6vv'

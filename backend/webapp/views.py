f3a8ab4945723633b3de157a3b4a7a84c9ad11b1
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2022-03-07 17:23:59 +0100

    working on /teamprogressstoday

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -82,6 +82,15 @@ def stepcount_today_team(team_id):
     return jsonify(res) # json.dumps(res, default=str, indent=4, sort_keys=True)
 
 
+@app.route('/teamprogresstoday/<team_id>', methods=['get'])
+def teamprogresstoday(team_id):
+    teamChallenge = TeamChallenge.select().where((TeamChallenge.team==team_id) & (TeamChallenge.date==datetime.now())).get()
+    res = list(StepCount.select().where(StepCount.team == teamChallenge).dicts())
+    print(res)
+    return jsonify(res)
+    
+    # teamChallenge = (TeamChallenge.select().where((TeamChallenge.team == team_id) & (TeamChallenge.date == datetime.now())).get())    
+    res = (StepCount.select(fn.SUM(StepCount.steps).alias('total_steps')).where((StepCount.team == team_id) & (StepCount.teamChallenge == teamChallenge)).get())  
 # @app.route('/private/')
 # @auth.login_required
 # def private_view():
@@ -161,5 +170,4 @@ def push_steps():
     steps.save()
     
     return Response(json.dumps(model_to_dict(steps, recurse=False), default=str, indent=4, sort_keys=True), mimetype='application/json')    
-    
     

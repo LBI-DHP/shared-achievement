fd80bdb6e121295c3f6e5155f5cb3fbb946a04a6
Eva-Maria Strumegger <eva-maria.strumegger@fh-salzburg.ac.at>
2022-04-04 11:31:45 +0200

    remove restart button

--- frontend/screens/NoInternetConnection.tsx
+++ frontend/screens/NoInternetConnection.tsx
@@ -1,8 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 import { style } from "../constants/Styles";
-import { Button } from "react-native-paper";
-import { Restart } from "fiction-expo-restart";
 
 export default function NoInternetConnection() {
   return (
@@ -10,19 +8,8 @@ export default function NoInternetConnection() {
       <Text style={style.heading}>Error 🚨</Text>
       <Text style={style.subheading}>
         It seems that you are not connected to the internet. Please check your
-        connection. 🌐
+        connection. And restart the app. 🌐
       </Text>
-      <Button
-        mode="contained"
-        style={{
-          marginTop: 10,
-        }}
-        onPress={() => {
-          Restart();
-        }}
-      >
-        🔄 Restart App
-      </Button>
     </View>
   );
 }

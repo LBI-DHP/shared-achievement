a9c08f2eab76363b0b02dc1e016e658a03c07122
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2022-04-25 17:00:49 +0200

    test

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -301,14 +301,14 @@ def manual():
     return render_template('setup_instructions.html', os=os, mode=progressCalculationMode)
 
 
-@app.route('/redirect_uri')
-def redirect_uri():
-    return "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"
+# @app.route('/redirect_uri')
+# def redirect_uri():
+#     return "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"
 
 ###### SURVEY
-@app.route('/consent', methods=['GET', 'POST'])
-def consent(user_id):
-    return render_template('consent.html')
+# @app.route('/consent', methods=['GET', 'POST'])
+# def consent(user_id):
+#     return render_template('consent.html')
     
 
 

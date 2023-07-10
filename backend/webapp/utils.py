214a2c4048c0b274e57985466d5163dc72475c4b
Alireza Fatehi <alireza.fatehi@dhp.lbg.ac.at>
2023-07-10 17:26:27 +0200

    debug timezone value in user_now and team_now functions and init from datetime.timezone.

--- backend/webapp/utils.py
+++ backend/webapp/utils.py
@@ -3,13 +3,15 @@ import datetime
 def usr_now(user_id):
     from models import User    
     usr = User.get_by_id(user_id)
-    usr_now = datetime.datetime.now(usr.timezone)
+    tz = datetime.timezone(offset=datetime.timedelta(days=0, seconds=0,hours=usr.timezone_offset), name=usr.timezone)
+    usr_now = datetime.datetime.now(tz)
     return usr_now
 
 def team_now(team_id):
     from models import Team
     team = Team.get_by_id(team_id)
-    team_now = datetime.datetime.now(team.timezone)
+    tz = datetime.timezone(offset=datetime.timedelta(days=0, seconds=0,hours=team.timezone_offset), name=team.timezone)
+    team_now = datetime.datetime.now(team.tz)
     return team_now
 
 def usr_today(user_id):
